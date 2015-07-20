1a483ba2075f479ffaf67e4c183c822f604d4a1e
Kevin Lowe <iamkevinlowe@gmail.com>
2015-07-19 20:51:10 -0700

    Installed Countdown amd Semdgrid gem.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,12 @@
 class Challenge < ActiveRecord::Base
   belongs_to :challenger, class_name: "Photo"
   belongs_to :challenged, class_name: "Photo"
+
+  before_save :default_length
+
+  private
+
+  def default_length
+    self.length ||= 6
+  end
 end
