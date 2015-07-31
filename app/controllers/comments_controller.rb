51a2a41d9f1a5065d80bd0a57bed488d3ae013bf
Kevin Lowe <iamkevinlowe@gmail.com>
2015-07-31 13:48:53 -0700

    Added scripts for comment create and destroy, and challenge photo preview

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,26 +2,35 @@ class CommentsController < ApplicationController
 
   def create
     photo = Photo.find(params[:photo_id])
-    comment = current_user.comments.build(comment_params)
-    comment.photo_id = photo.id
-    authorize comment
-    if comment.save
+    @comment = current_user.comments.build(comment_params)
+    @comment.photo_id = photo.id
+    @new_comment = Comment.new(photo_id: photo.id)
+    authorize @comment
+    if @comment.save
       flash[:notice] = "Comment created successfully."
     else
       flash[:error] = "Something went wrong. Please try again."
     end
-    redirect_to :back
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
-    comment = Comment.find(params[:id])
-    authorize comment
-    if comment.destroy
+    @comment = Comment.find(params[:id])
+    authorize @comment
+    if @comment.destroy
       flash[:notice] = "Comment was deleted successfully."
     else
       flash[:error] = "Something went wrong. Please try again."
     end
-    redirect_to :back
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
